9b1799b0f7296519984317968ec005ea9408b3fb
Vahe <vah.aghajanyan@gmail.com>
2023-04-28 16:47:16 +0400

    First commit: Python basics and data types.

---
+++ 1. Basics.py
@@ -0,0 +1,16 @@
+# Hello World and variable declaration
+
+print('Hello World!')
+
+a = 10
+b = 20
+
+print(a,b)
+
+# Change variables' values
+
+temp = a
+a = b
+b = temp
+
+print(a, b)
---
+++ 2. Data Types.py
@@ -0,0 +1,23 @@
+# Integer
+a = 42
+
+#Float
+b = 3.14159
+
+#Boolean
+bool = True
+
+#String
+str = 'Text Example'
+
+#List
+fruits = ["apple", "banana", "cherry"]
+
+#Tuple
+coordinates = (10, 20)
+
+#Set
+numbers = {1, 2, 3, 4, 5}
+
+#Dictionary
+person = {"name": "John Doe", "age": 30, "city": "New York"}
