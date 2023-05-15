6517e10d2a6ff85abf81911f612b61c5edd03423
Vahe <vah.aghajanyan@gmail.com>
2023-05-15 22:16:20 +0400

    13. OOP - Basic class creation.

--- 12. Sets.py
+++ 12. Sets.py
@@ -1,5 +1,6 @@
 my_set = {1, 2, 3, 4, 5}
 my_set.add(6)
+print(my_set.__contains__(2))
 my_set.remove(3)
 
 if 4 in my_set:
---
+++ 13. OOP.py
@@ -0,0 +1,9 @@
+class Person:
+    pass
+
+p1 = Person()
+p1.name = "Vahe"
+p1.surname = "Aghajanyan"
+p1.place_of_birth = "Armenia"
+
+print(f'Person: Name - {p1.name}, Surname - {p1.surname}, {p1.place_of_birth}')
